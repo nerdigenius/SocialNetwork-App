349f002b98d1db723c753154bdf8cc917ffe67fa
nerdgenius <Ashiq.racer@gmail.com>
2020-03-28 14:27:15 +0600

    routing done

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const cors = require('cors');
 const users=require('./routes/api/users');
 const profile=require('./routes/api/profile');
 const posts=require('./routes/api/posts');
-
+const bodyParser = require('body-parser');
 
 
 require('dotenv').config();
 const app = express();
 const uri =process.env.ATLAS_URI;
+
+//body-parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex:true,useUnifiedTopology: true});
 const port = process.env.PORT || 5000;
