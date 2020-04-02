f76e2fec48081df7ac58f5185503e9fbe392f722
nerdgenius <Ashiq.racer@gmail.com>
2020-04-02 18:45:39 +0600

    delete education login experience route done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -216,4 +216,67 @@ router.post('/education',passport.authenticate('jwt',{session:false}),(req,res)=
     })
 })
 
+//@route delete to api/profile/experience
+//@desc delete experience from profile
+//@access Private
+
+router.delete('/experience/:exp_id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+   
+
+    Profile.findOne({user:req.user.id})
+    .then(profile=>{
+        //Get remove index
+        const removeIndex = profile.experience
+        .map(item=>item.id)
+        .indexOf(req.params.exp_id);
+
+        //splice out of array
+        profile.experience.splice(removeIndex,1);
+
+        //save
+
+        profile.save().then(profile=>res.json(profile))
+    })
+    .catch(err=>res.status(404).json(err))
+})
+
+//@route delete to api/profile/education
+//@desc delete education from profile
+//@access Private
+
+router.delete('/education/:edu_id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+   
+
+    Profile.findOne({user:req.user.id})
+    .then(profile=>{
+        //Get remove index
+        const removeIndex = profile.education
+        .map(item=>item.id)
+        .indexOf(req.params.edu_id);
+
+        //splice out of array
+        profile.education.splice(removeIndex,1);
+
+        //save
+
+        profile.save().then(profile=>res.json(profile))
+    })
+    .catch(err=>res.status(404).json(err))
+})
+
+//@route delete to api/profile
+//@desc delete user and profile
+//@access Private
+
+router.delete('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
+   
+
+    Profile.findOneAndRemove({user:req.user.id})
+    .then(()=>{
+        User.findOneAndRemove({_id:req.user.id})
+        .then(()=>res.json({success:true}))
+    })
+    
+})
+
 module.exports = router;
