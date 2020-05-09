c68eff850e92eda2c534f387e493232a20288731
nerdgenius <Ashiq.racer@gmail.com>
2020-05-09 16:56:16 +0600

    prepared for deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const profile=require('./routes/api/profile');
 const posts=require('./routes/api/posts');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-
+const path=require('path');
 
 require('dotenv').config();
 const app = express();
@@ -31,6 +31,15 @@ require('./config/passport')(passport);
 app.use('/api/users',users);
 app.use('/api/profile',profile);
 app.use('/api/posts',posts);
+
+//serve static assests if in production
+if(process.env.NODE_ENV==='produciton'){
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 
 
